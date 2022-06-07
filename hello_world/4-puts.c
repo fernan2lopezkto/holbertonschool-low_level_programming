42193c91c087e2ea0c31867bbec21efec6186fa5
Juan Rey <5079@holbertonschool.com>
2022-06-07 06:53:09 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main(void)
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)/*principal function*/
+/*
+ *imprimiremos en pantalla con puts / we will print to screen with puts
+ */
 {
 	char TEXT[] = "\"Programming is like building a multilingual puzzle";
 
