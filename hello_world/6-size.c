afbe0fab19a9c01a59c3652c2bb0644e59394f45
Juan Rey <5079@holbertonschool.com>
2022-06-07 16:19:20 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	double douType;
 	float floatType;
 
-	printf("Size of a char: %lu byte(s) \n", sizeof(charType));
-	printf("Size of an int: %lu byte(s) \n", sizeof(intType));
-	printf("Size of a long int: %lu byte(s) \n", sizeof(LiType));
-	printf("Size of a long long int: %lu byte(s) \n", sizeof(douType));
-	printf("Size of a float: %lu byte(s) \n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(LiType));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(douType));
+	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
