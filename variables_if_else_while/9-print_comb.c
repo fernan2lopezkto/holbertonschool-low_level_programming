7c998d7e8abafdf376f1bbe356fba70fb5e4bd8e
Juan Rey <5079@holbertonschool.com>
2022-06-08 07:26:28 -0700

    9-print_comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,12 +12,14 @@ int main(void)
 	int x;
 
 	for (x = '0'; x <= '9'; x++)
+	{
 	putchar(x);
-		if (x != 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		putchar('\n');
+	if (x != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
 	return (0);
 }
