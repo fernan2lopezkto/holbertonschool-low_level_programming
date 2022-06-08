896afa4378099aeb553fa2c217730c65d58cef0a
Juan Rey <5079@holbertonschool.com>
2022-06-08 06:28:08 -0700

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* here is my code */
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
