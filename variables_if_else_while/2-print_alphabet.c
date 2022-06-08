1925224a427b751d1cb28b638e48f87afa1b178e
Juan Rey <5079@holbertonschool.com>
2022-06-07 19:40:55 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* here is my code */
+	char x[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 27; i++)
+	putchar(x);
+	return (0);
+}
