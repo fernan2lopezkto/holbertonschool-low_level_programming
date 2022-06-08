45dad138807f44eed495e2f1641c584f0efe9883
Juan Rey <5079@holbertonschool.com>
2022-06-08 05:27:42 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* here is my code */
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+	putchar(x);
+
+	for (x = 'A'; x <= 'Z'; x++)
+
+	putchar(x);
+	putchar('\n');
+	return (0);
+}
