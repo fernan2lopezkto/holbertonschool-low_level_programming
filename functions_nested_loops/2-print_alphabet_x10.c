5a13e83b998c579df061e182d5417ce09f920dad
Juan Rey <5079@holbertonschool.com>
2022-06-09 11:56:19 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
 /**
  * print_alphabet_x10 - print alphabet 10 times
  *
- * return: void 
+ * return: void
  */
 void print_alphabet_x10(void)
 {
+	char i;
+
+	for (i = 0; x < 5; i++)
+	{
 	char x;
 
-	for (x = 0; x < 5; x++)
-
-	print_alphabet();
+	for (x = 'a'; x <= 'z'; x++)
+	_putchar(x);
+	_putchar('\n');
+	}
 }
