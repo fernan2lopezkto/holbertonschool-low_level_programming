f91882222c2f3b4ee08db90b48b4a43a92734a2d
Juan Rey <5079@holbertonschool.com>
2022-06-09 12:32:52 -0700

    3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * return: void
+ */
+int _islower(int c);
+{
+	if (c > 97 && c < 122)
+	return(1);
+
+	else
+	return(0);
+}
