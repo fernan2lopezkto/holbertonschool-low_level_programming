abf9a5bcea3b49dd07b9d7e49f61bb9303e52142
Juan Rey <5079@holbertonschool.com>
2022-06-09 10:33:42 -0700

    alphabet 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+	_putchar(x);
+	_putchar('\n');
+	return (0);
+}
