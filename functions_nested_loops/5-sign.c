482dbf3434ecdf263ddeadefe9b93517ec7763a0
Juan Rey <5079@holbertonschool.com>
2022-06-09 18:55:58 -0700

    5-sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _print_sign - test sign
+ * @n: is te parameter of main.c
+ * Return: void
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+		return (1);
+
+	if (n == 0)
+		return (0);
+
+	if (n < 0)
+		return (-1);
+
+	return (0);
+}
