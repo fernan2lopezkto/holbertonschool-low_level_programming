74c3a440e1817f6117535e4b42fef9947dba9bc2
Juan Rey <5079@holbertonschool.com>
2022-06-10 07:34:36 -0700

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  * print_sign -test sign
+  * @n: is te parameter of main.c
+  * Return: void
+  */
+int _abs(int)
+{
+	int m;
+	if (m >= 0)
+		return (m);
+
+	if (m < 0)
+		return (-m);
+
+	return (0);
+}
