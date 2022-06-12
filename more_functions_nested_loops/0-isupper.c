060fca5fa15ec518c3554e9387ef137bb218fc65
Juan Rey <5079@holbertonschool.com>
2022-06-11 19:20:11 -0700

    upper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,13 @@
 #include "main.h"
 
-
+/**
+ * _isupper -funcion que detecta mayuscula
+ * @c : is the parameters
+ * Return: int
+ */
 int _isupper(int c)
 {
-	if(c > 64 && c < 91)
+	if (c > 64 && c < 91)
 		return (1);
 
 	return (0);
