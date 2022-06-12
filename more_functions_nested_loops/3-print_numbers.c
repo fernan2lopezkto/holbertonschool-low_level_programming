81edc4fff0772bc582a3d83c2884ed4bca51346d
Juan Rey <5079@holbertonschool.com>
2022-06-12 06:50:45 -0700

    3_print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul -funcion que detecta mayuscula
+ * @a : to multiplicate parameter
+ * @b : multiplicator parameter
+ *
+ * Return: int
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		_putchar(i + '0');
+
+	_putchar('\n');
+}
