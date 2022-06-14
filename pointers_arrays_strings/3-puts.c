b266ef90d738e58d4492b609d424eed3d684f31f
Juan Rey <5079@holbertonschool.com>
2022-06-14 11:43:49 -0700

    ejercicio_3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,15 +1,18 @@
 #include "main.h"
 
+/**
+ * _puts - print string
+ *@str: string
+ */
 void _puts(char *str)
 {
-int i = 0;
+	int i = 0;
 
-        while (*str != '\0')
-        {
-                ++i;
-                ++str;
+	while (*str != '\0')
+	{
 		_putchar(*str);
-        }
-
+		i++;
+		str++;
+	}
 	_putchar('\n');
 }
