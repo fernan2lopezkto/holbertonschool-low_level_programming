606552e2aa8bc260aea7846eb7399ecd3720ed47
Juan Rey <5079@holbertonschool.com>
2022-06-16 09:09:32 -0700

    coppy_pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+# include "main.h"
+
+/**
+ * *_strcpy - is a pointer functions?
+ * @dest: destino del coppy
+ * @src: from we can coppy
+ * Return: char poiter
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
