9bda95d6d598321179178b70c8698e75481f0395
Juan Rey <5079@holbertonschool.com>
2022-06-16 08:25:39 -0700

    8_print_Array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - the function
+ *@a: array
+ *@n: array long
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)/*quitarle coma y espacio al ultimo numero*/
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	printf("\n");
+}
