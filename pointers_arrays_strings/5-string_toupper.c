7ed4e55ffda760376805583de4617160dc7cff59
Juan Rey <5079@holbertonschool.com>
2022-06-17 17:22:41 -0700

    Lower_Apper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *string_toupper - convert the low to uppercase
+ * @s: pointer positions
+ * Return: char
+ */
+char *string_toupper(char *s)
+{
+	int back = 0;
+
+	while (*s != '\0')
+	{
+		if (*s > 97 && *s < 122)/*segment lowercase*/
+		{
+			*s = *s - 32;
+		}
+		s++;
+		back++;/*to know how much back*/
+	}
+
+	return (s - back);/*move pointer to start*/
+}
