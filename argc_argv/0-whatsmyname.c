4894ef6a57afb2386517a0f288c5a60179c8778a
Juan Rey <5079@holbertonschool.com>
2022-06-21 11:15:15 -0700

    What is my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - what is my name?
+ * @argc: total arguments
+ * @argv: array whit arguments saved
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
