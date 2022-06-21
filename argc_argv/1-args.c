f4b9156fe890cb86b00c288c3f84d921d3eea806
Juan Rey <5079@holbertonschool.com>
2022-06-21 14:41:47 -0700

    print count variable

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - what is my name?
+ * @argc: total arguments
+ * @argv: array whit arguments saved
+ * Return: int
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
