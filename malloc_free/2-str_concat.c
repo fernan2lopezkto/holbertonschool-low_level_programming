4e9a9103455f5311a9d7fd28caeee5229102a682
Juan Rey <5079@holbertonschool.com>
2022-06-23 14:17:19 -0700

    concatenation string if are null convert in empty string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -6,24 +6,49 @@
 /**
  * *str_concat - concatenate array
  * @s1: is the string one from main file
- * @s2:
+ * @s2: i
  * Return: char
  */
 char *str_concat(char *s1, char *s2)
 {
 	char *pr;
-	int size1, size2;
+	int size1, size2, ran;
 
-	size1 = strlen(s1);
-	size2 = strlen(s2);
 
-	pr = malloc((size1 + size2) * sizeof(char));
-
-	if (pr == NULL)
-		return (0);
-
-	strcpy(pr, s1);
-	strcpy(pr + size1, s2);
-
+	if (s1 == NULL && s2 == NULL)
+	{
+		pr = malloc(1 * sizeof(char));
+		pr = '\0';
+		return (pr);
+	}
+	if (s1 == NULL)
+	{
+		size2 = strlen(s2);
+		pr = malloc((size2 + 1) * sizeof(char));
+		if (pr == NULL)
+			return (0);
+		for (ran = 0; ran <= size2; ran++)
+			pr[ran] = s2[ran];
+		return (pr);
+	}
+	if (s2 == NULL)
+	{
+		size1 = strlen(s1);
+		pr = malloc((size1 + 1) * sizeof(char));
+		if (pr == NULL)
+			return (0);
+		for (ran = 0; ran <= size1; ran++)
+			pr[ran] = s1[ran];
+		return (pr);
+	}
+	else
+	{
+		size1 = strlen(s1);
+		size2 = strlen(s2);
+		pr = malloc((size2 + size1) * sizeof(char));
+			if (pr == NULL)
+		strcpy(pr, s1);
+		strcpy(pr + size1, s2);
+	}
 	return (pr);
 }
