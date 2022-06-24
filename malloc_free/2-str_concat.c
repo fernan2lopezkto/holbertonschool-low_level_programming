41e95417b8df6bca6df11b73fa169e1541b52455
Juan Rey <5079@holbertonschool.com>
2022-06-23 17:34:06 -0700

    concatenatetion of thr string

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-
 /**
  * *str_concat - concatenate array
  * @s1: is the string one from main file
@@ -12,48 +10,24 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *pr;
-	int size1, size2, ran;
+	int size1, size2, i, j;
 
-
-	if (s1 == NULL && s2 == NULL)
-	{
-		pr = malloc(1 * sizeof(char));
-		if (pr == NULL)
-			return (0);
-		pr[0] = '\0';
-		return (pr);
-	}
 	if (s1 == NULL)
-	{
-		size2 = strlen(s2);
-		pr = malloc((size2 + 1) * sizeof(char));
-		if (pr == NULL)
-			return (0);
-		for (ran = 0; ran <= size2; ran++)
-			pr[ran] = s2[ran];
-		return (pr);
-	}
+		s1 = "";
 	if (s2 == NULL)
+		s2 = "";
+	size1 = strlen(s1);
+	size2 = strlen(s2);
+	pr = malloc((size1 + size2 + 1) * sizeof(char));
+	if (pr == NULL)
+		return (NULL);
+	for (i = 0; i < size1; i++)
+		pr[i] = s1[i];
+	for (j = 0; j < size2; j++)
 	{
-		size1 = strlen(s1);
-		pr = malloc((size1 + 1) * sizeof(char));
-		if (pr == NULL)
-			return (0);
-		for (ran = 0; ran <= size1; ran++)
-			pr[ran] = s1[ran];
-		return (pr);
+		pr[i] = s2[j];
+		i++;
 	}
-	else
-	{
-		size1 = strlen(s1);
-		size2 = strlen(s2);
-		pr = malloc((size2 + size1 + 1) * sizeof(char));
-		if (pr == NULL)
-		{
-			return (0);
-		}
-		strcpy(pr, s1);
-		strcpy(pr + size1, s2);
-	}
+	pr[i + j] = '\0';
 	return (pr);
 }
