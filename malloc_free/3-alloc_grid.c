080f58fbebf4e2523c8a2f310f99f337dbf337a6
Juan Rey <5079@holbertonschool.com>
2022-06-23 20:09:27 -0700

    make a array whith pointers

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - make the array whit pointers
+ * @width: width
+ * @height: height
+ * Return: int
+ */
+int **alloc_grid(int width, int height)
+{
+	int **mirta;
+	int i, j;
+
+	if (width < 1)
+		return (NULL);
+	if (height < 1)
+		return (NULL);
+
+	mirta = malloc(height * sizeof(int));
+	if (mirta == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		mirta[i] = malloc(width * sizeof(int));
+		if (mirta == NULL)
+			return (NULL);
+		for (j = 0; j < width; j++)
+		{
+			mirta[i][j] = 0;
+		}
+	}
+	return (mirta);
+}
