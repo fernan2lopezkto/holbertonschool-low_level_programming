60dcd67267a2cc2be86935b8240527b7a4aaf325
Juan Rey <5079@holbertonschool.com>
2022-06-26 16:24:05 -0700

    more malloc whit exit comand

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
+ * *malloc_checked - function to create space
+ * @b: amount of space required
+ * Return: pointer
  */
 void *malloc_checked(unsigned int b)
 {
 	void *pr;
 
-	pr = malloc (b);
+	pr = malloc(b);
 	if (pr == NULL)
-		exit (98);
+		exit(98);
 	return (pr);
 }
