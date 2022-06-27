7becea9cb10bafb0d08f6ef05b5f31bf4367951c
Juan Rey <5079@holbertonschool.com>
2022-06-26 19:28:07 -0700

    especial array

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ **array_range - function
+ *@min: initial number
+ *@max: stop namber
+ *Return: int
+ */
+int *array_range(int min, int max)
+{
+	int size, i = 0, *pr;
+
+	if (min > max)
+		return (NULL);
+	size = max - min;
+	pr = malloc(size * sizeof(int));
+	if (pr == NULL)
+		return (NULL);
+	for (; min <= max; min++)
+	{
+		pr[i] = min;
+		i++;
+	}
+	return (pr);
+}
