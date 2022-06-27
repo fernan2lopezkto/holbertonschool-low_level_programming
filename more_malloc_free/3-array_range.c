a33f4fa88d86731dd554e6e606be8f63a1508f11
Juan Rey <5079@holbertonschool.com>
2022-06-26 19:31:03 -0700

    especial array

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	size = max - min;
+	size = max - min + 1;
 	pr = malloc(size * sizeof(int));
 	if (pr == NULL)
 		return (NULL);
