30278c1d8fe8b7190b86033737587b409d7c3b54
Juan Rey <5079@holbertonschool.com>
2022-06-26 17:12:31 -0700

    concatenate two string whit n byte of ter2

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *string_nconcat - concatenate two strings
+ * @s1: str 1 string
+ * @s2: str 2 string
+ * @n: long of char to coppy of str2
+ * Return: char
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *pr;
+	unsigned int  size2, i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	size2 = strlen(s2);
+	if (n >= size2)
+		n = size2;
+
+	pr = malloc((strlen(s1) + size2 + 1) * sizeof(char));
+	if (pr == NULL)
+		return (NULL);
+
+	for (i = 0; i < strlen(s1); i++)
+		pr[i] = s1[i];
+	for (j = 0; j < size2; j++)
+	{
+		pr[i] = s2[j];
+		i++;
+	}
+	pr[i + j] = '\0';
+	return (pr);
+}
