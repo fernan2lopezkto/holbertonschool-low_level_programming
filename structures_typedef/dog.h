ee2d8ed67459e7d2712a7d53fbfcc1d0d9277965
Juan Rey <5079@holbertonschool.com>
2022-06-28 19:03:09 -0700

    struct in header

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - fers truct about dog
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dogs owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
