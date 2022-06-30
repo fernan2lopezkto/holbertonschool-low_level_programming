dd937ec9d4cedca453bf88e859683df35fbab62a
Juan Rey <5079@holbertonschool.com>
2022-06-30 14:42:52 -0700

    to send i of the array to function to detect mach

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,10 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = -1;
 
 	if (array != NULL && size > 0 && cmp != NULL)
+	{
 		for (i = 0; i < size; i++)
 		{
 			if ((*cmp)(array[i]) != 0)
 				break;
 		}
+	}
 	return (i);
 }
