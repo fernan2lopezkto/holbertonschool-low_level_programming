d1311d7e6ca412b83a7f3a5fa03529ffc8961744
Juan Rey <5079@holbertonschool.com>
2022-06-30 09:44:14 -0700

    new dog from pointer

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,39 +10,39 @@ int _strlen(char *s);
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *pr;
-        int size_n, size_o, i;
+	dog_t *pr;
+	int size_n, size_o, i;
 
-        if (name == NULL)
-                return (NULL);
-        if (owner == NULL)
-                return (NULL);
-        size_n = _strlen(name);
-        size_o = _strlen(owner);
-        pr = malloc(sizeof(dog_t));
-        if (pr == NULL)
-                return (NULL);
-        pr->name = malloc(size_n + 1);
-        if (pr->name == NULL)
-        {
-                free(pr->name);
-                free(pr);
-                return (NULL);
-        }
-        pr->owner = malloc(size_o + 1);
-        if (pr->owner == NULL)
-        {
-                free(pr->name);
-                free(pr->owner);
-                free(pr);
-                return (NULL);
-        }
-        pr->age = age;
-        for (i = 0; i <= size_n; i++)
-                pr->name[i] = name[i];
-        for (i = 0; i <= size_o; i++)
-                pr->owner[i] = owner[i];
-        return (pr);
+	if (name == NULL)
+		return (NULL);
+	if (owner == NULL)
+		return (NULL);
+	size_n = _strlen(name);
+	size_o = _strlen(owner);
+	pr = malloc(sizeof(dog_t));
+	if (pr == NULL)
+		return (NULL);
+	pr->name = malloc(size_n + 1);
+	if (pr->name == NULL)
+	{
+		free(pr->name);
+		free(pr);
+		return (NULL);
+	}
+	pr->owner = malloc(size_o + 1);
+	if (pr->owner == NULL)
+	{
+		free(pr->name);
+		free(pr->owner);
+		free(pr);
+		return (NULL);
+	}
+	pr->age = age;
+	for (i = 0; i <= size_n; i++)
+		pr->name[i] = name[i];
+	for (i = 0; i <= size_o; i++)
+		pr->owner[i] = owner[i];
+	return (pr);
 }
 /**
  * _strlen - function for determinate string longer
@@ -51,9 +51,9 @@ dog_t *new_dog(char *name, float age, char *owner)
  */
 int _strlen(char *s)
 {
-        int i;
+	int i;
 
-        for (i = 0; *s != '\0'; i++)
-                s++;
-        return (i);
+	for (i = 0; *s != '\0'; i++)
+		s++;
+	return (i);
 }
