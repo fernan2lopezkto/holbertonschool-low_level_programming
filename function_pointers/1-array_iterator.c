895626a02abacfb694efb1e41d772be3bb1eb71a
Juan Rey <5079@holbertonschool.com>
2022-06-30 13:06:46 -0700

    function to do any things whit array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+/**
+ *array_iterator - function for ran a array
+ *@array: is the array
+ *@size: is array size
+ *@action: function to do whit array
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			(*action)(array[i]);
+	}
+}
