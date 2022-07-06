26c7a231e435f007a4b4fed3fea975fc979f776c
Juan Rey <5079@holbertonschool.com>
2022-07-06 07:02:02 -0700

    not mull

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+
+
+/**
+ *op_add - function
+ *@a: int a
+ *@b: int b
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - function
+ *@a: int a
+ *@b: int b
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - function
+ *@a: int a
+ *@b: int b
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - function
+ *@a: int a
+ *@b: int b
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - function
+ *@a: int a
+ *@b: int b
+ *Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
