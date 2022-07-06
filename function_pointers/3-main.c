ab3617ae8d393795f157192c386871eb96269b15
Juan Rey <5079@holbertonschool.com>
2022-07-06 07:26:22 -0700

    not mull

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -3,21 +3,26 @@
 #include "3-calc.h"
 #include <string.h>
 #include <stdlib.h>
-
+/**
+ * main - principal function
+ * @argc: arguments sumary
+ * @argv: argument vector
+ * Return: int
+ */
 int main(int argc, char *argv[])
 {
 	int r;
 
 	if (argc != 4)
 	{
-		printf("Error");
+		printf("Error\n");
 		exit(98);
 
 	}
 	if (((*argv[2] == '/') || (*argv[2] == '%')) && *argv[3] == '0')
 	{
-			printf("Error\n");
-			exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 	if ((*(get_op_func(argv[2]))) && (strlen(argv[2]) == 1))
 	{
