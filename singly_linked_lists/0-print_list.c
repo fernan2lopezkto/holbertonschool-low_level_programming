c1cc9626e4fda2eaf1ab684810cb5ed849141210
Juan Rey <5079@holbertonschool.com>
2022-07-13 07:20:10 -0700

    exercise 0 ok

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,6 +12,7 @@ size_t print_list(const list_t *h)
 	size_t counter = 0;
 
 	while (h)
+	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
@@ -19,8 +20,9 @@ size_t print_list(const list_t *h)
 		else
 		{
 			printf("[%d] (%s)\n", h->len, h->str);
-			counter++;
-			h = h->next;
 		}
+		h = h->next;
+		counter++;
+	}
 	return (counter);
 }
