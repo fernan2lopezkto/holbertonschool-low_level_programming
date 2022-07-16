c45d15876d48b5a65b6c9230474d2abe3f488867
Juan Rey <5079@holbertonschool.com>
2022-07-15 18:28:38 -0700

    delete linked list and set head to NULL

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - free the memory
+ * @head: linked list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
+	}
+	*head = NULL;
+}
