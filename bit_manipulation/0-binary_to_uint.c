61dc9d967f3d5d3618e564c5e9fb6f60d5f6dd74
Juan Rey <5079@holbertonschool.com>
2022-07-19 13:40:22 -0700

    convert from bin to dec

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stddef.h>
+
+unsigned int pow_2(unsigned int position);
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b:  is pointing to a string of 0 and 1 chars
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int bin = 0, position = 0;
+	int stop = 0;
+	
+	if (b == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (b[stop] != '\0')
+			stop++;
+
+		while (stop >= 0)
+		{
+			stop--;
+			if (b[stop] == 49)
+				bin = bin + pow_2(position);
+			position++;
+
+		}
+			
+	}
+
+	return (bin);
+}
+
+unsigned int pow_2(unsigned int position)
+{
+	unsigned int i, pow = 2;
+
+	if (position == 0)
+		return (1);
+
+	if (position == 1)
+		return (2);
+
+	for (i = 1; i < position; i++)
+		pow = pow * 2;
+
+	return (pow);
+}
---
+++ bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this will bw a readme
