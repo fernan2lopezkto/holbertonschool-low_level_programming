634d39f54ec0d6cb5bbda545dbeb105deeb3566a
Juan Rey <5079@holbertonschool.com>
2022-07-21 15:02:25 -0700

    test task 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of list
+ * @h: pointer to dlistint_t
+ * Return: n of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+	return (counter); 	
+}
