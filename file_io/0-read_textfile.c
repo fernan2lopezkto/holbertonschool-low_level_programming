953d8e0a3d8d1df9e6efa5142565756cc1d15c3b
Juan Rey <5079@holbertonschool.com>
2022-08-17 16:31:04 -0700

    add code to main

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,7 +1,35 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-
+/**
+ * read_textfile - read a text file
+ *
+ * @filename: namber of the file
+ * @letters: is the number of letters it should read and print
+ *
+ * Return:  the actual number of letters it could read and print
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf = NULL;
+	int fd = open(filename, O_RDWR);
+	
+	if(fd == -1)
+		return (0);
+
+	buf = malloc(letters + sizeof(char));
+	if (!buf)
+		retun(0);
+
+	read(fd, buf, letters);
+
+	printf("buf is %s", buf);
+
+	close(fd);
+	free(buf);
+
+	return (0);
 
 }
