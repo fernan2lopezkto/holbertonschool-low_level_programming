5c8f3932efa37e53841233391a8ef2d8b994b225
Juan Rey <5079@holbertonschool.com>
2022-08-17 15:54:46 -0700

    create folder and any files. -> 0-main.c -> 0-read_textfile.c -> README.md -> Requiescat -> _putchar.c -> main.h

---
+++ file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*INCLUDES*/
+#include <unistd.h>
+
+
+/*--PROTOTYPES--*/
+/*  tasck       */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+/*  helpers     */
+int _putchar(char c);
+
+#endif
