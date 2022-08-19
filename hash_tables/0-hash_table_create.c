c3cab3f7eaf1a53f87a46ad19da8fe1a9899bf47
Juan Rey <5079@holbertonschool.com>
2022-08-19 12:22:13 -0700

    add code to  0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,5 +1,18 @@
 #include <hash_tables.h>
 
+
+/**
+ * hash_table_create -  that creates a hash table
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(void *) * size);
+	if (!ht)
+		return (NULL)
+
+	return (ht);
 }
