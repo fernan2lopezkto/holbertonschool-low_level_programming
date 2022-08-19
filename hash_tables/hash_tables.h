0515a64b772f2695554209c2022301f4040e9315
Juan Rey <5079@holbertonschool.com>
2022-08-19 15:32:15 -0700

    ad files to task 2 -> 1-djb2.c -> hash_tables.h (fix this)

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -45,7 +45,7 @@ typedef struct hash_table_s
 /*   ---PROTOTYPES---  */
 /*         TASKS       */
 hash_table_t *hash_table_create(unsigned long int size);
-
+unsigned long int hash_djb2(const unsigned char *str);
 
 /*       HELPERS      */
 
