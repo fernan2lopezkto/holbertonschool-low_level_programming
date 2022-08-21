1de1a7ddaa2c71b240452ddce4e2729e1624606a
Juan Rey <5079@holbertonschool.com>
2022-08-21 08:24:11 -0700

    fix details

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,18 +12,34 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
+	hash_node_t *new_list_node = NULL;
 
 
 	index = key_index((const unsigned char *)key, ht->size);
 
-
-	ht->array[index] = malloc(sizeof(hash_node_t));
-	if (!ht->array[index])
+	new_list_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	if (new_list_node == NULL)
+	{
 		return (0);
+	}
+	new_list_node->key = strdup((char *)key);
+	new_list_node->value = strdup((char *)value);
 
-	ht->array[index]->key = strdup(key);
-	ht->array[index]->value = strdup(value);
-
+	if (ht->array[index] == NULL || (strcmp(ht->array[index]->key, key) == 0))
+	{
+		if (ht->array[index])
+		{
+			free(ht->array[index]->key);
+			free(ht->array[index]->value);
+			free(ht->array[index]);
+		}
+		ht->array[index] = new_list_node;
+	}
+	else
+	{
+		new_list_node->next = ht->array[index];
+		ht->array[index] = new_list_node;
+	}
 
 	return (1);
 }
